c1cf846339df76dae8c39ce40eeeb1947c38aa80
Viictoor29 <vicpincor29@gmail.com>
2025-10-10 18:32:55 +0200

    P1

---
+++ src/main/java/es/unex/mdai/FitReserve/FitReserveApplication.java
@@ -0,0 +1,13 @@
+package es.unex.mdai.FitReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/unex/mdai/FitReserve/FitReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.mdai.FitReserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
