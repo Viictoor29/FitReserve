14899e9d7cc42e73dd8ff804c20dea27241b8735
Viictoor29 <vicpincor29@gmail.com>
2025-10-13 15:01:21 +0200

    Creación de Maquinaria
    Signed-off-by: Viictoor29 <vicpincor29@gmail.com>

---
+++ src/main/java/es/unex/mdai/FitReserve/data/model/Maquinaria.java
@@ -0,0 +1,51 @@
+package es.unex.mdai.FitReserve.data.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Maquinaria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_maquinaria;
+
+    @NotNull
+    @Column(unique = true, nullable = false, length = 20)
+    private String nombre;
+
+    @NotNull
+    @Column(nullable = false)
+    int cantidad_total;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    public long getId_maquinaria() {
+        return id_maquinaria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad_total() {
+        return cantidad_total;
+    }
+
+    public void setCantidad_total(int cantidad_total) {
+        this.cantidad_total = cantidad_total;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
--- src/main/java/es/unex/mdai/FitReserve/data/model/Sala.java
+++ src/main/java/es/unex/mdai/FitReserve/data/model/Sala.java
@@ -65,6 +65,4 @@ public class Sala {
         this.descripcion = descripcion;
     }
 
-
-
 }
