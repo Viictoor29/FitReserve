9d30cf33c712cd8c2a8ed644834e7b66ed252e44
Viictoor29 <vicpincor29@gmail.com>
2025-11-28 09:36:07 +0100

    Cambios
    Signed-off-by: Viictoor29 <vicpincor29@gmail.com>

--- src/main/java/es/unex/mdai/FitReserve/controller/RedirectUsrController.java
+++ src/main/java/es/unex/mdai/FitReserve/controller/RedirectUsrController.java
@@ -1,14 +1,23 @@
-// java
 package es.unex.mdai.FitReserve.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class EntrenadorController {
+public class RedirectUsrController {
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "adminPage";
+    }
 
     @GetMapping("/entrenador")
     public String entrenadorPage() {
         return "entrenadorPage";
+    }
+
+    @GetMapping("/cliente")
+    public String clientePage() {
+        return "clientePage";
     }
 }
